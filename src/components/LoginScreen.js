cd8ee8fb3d0eb1d98afaeb4dc69c4d88ac93591f
Bryn Beaudry <babeaudry@gmail.com>
2018-01-29 18:27:28 -0800

    Modified login screen to show cabcheap logo

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,42 +1,198 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import { Container, Header, H1, H2, Content, Form, Item, Input, Button, Text, Label, Icon, Thumbnail } from 'native-base';
+import axios from 'axios';
+import querystring from 'query-string';
+import Constants from '../config/constants';
+import genericAlert from '../util/genericAlert';
+import safeExecute from '../util/safeExecute'
+import Spinner from 'react-native-loading-spinner-overlay'
+import { storeAccessToken, storeApplicationUserId, storeRefreshToken, storeCredentials } from "../config/PotAuth";
+import logo from "../assets/CCLogo640x480.png"
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+export default class LoginScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            correctPassword: false,
+            loading : false
+        };
+    }
+
+    _navigateToRegister = () => {
+        this.props.navigation.navigate('Register')
+    }
+
+    _login = async () => {
+        try {
+            this.setState({ loading : true })
+
+            if (this.state.username === "" && this.state.password === "") {
+                genericAlert("Required Field", "Please enter an email and a password.");
+                return;
+            } else if (this.state.email === "") {
+                genericAlert("Required Field", "Please enter an email.");
+                return;
+            } else if (this.state.password === "") {
+                genericAlert("Required Field", "Please enter a password.");
+                return;
+            }
+
+            let body = querystring.stringify({
+                response: 'code',
+                grant_type: 'password',
+                username: this.state.email,
+                password: this.state.password   
+            });
+
+            let response = await axios({
+                method: 'post',
+                url: `${Config.url}connect/token`,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                data: body
+            });
+
+            if (response.status === 200) { // OK success
+                this.setState({ loading : false })
+                await storeAccessToken(response.data.access_token);
+                await storeApplicationUserId(response.data.id_token);
+                await storeRefreshToken(response.data.refresh_token)
+                await storeCredentials(this.state.email, this.state.password)
+                this.props.navigation.navigate('Main');
+            } else {
+                genericAlert("Request Error", "An error occurred during login.");
+            }
+        } catch (error) {
+            genericAlert("Oops!", error.response.data.error_description);
+        } finally {
+            this.setState({ loading : false })
+        }
+    };
+
+    render() {
+        return (
+            <Container style={loginStyles.container}>
+                {/* <H1 style={loginStyles.brand}>{Constants.APP_NAME}</H1> */}
+                <Image style={{flex:.5, flexDirection: 'column', height: undefined, width: undefined}} resizeMode='contain' source={logo}/>
+                <Spinner 
+                    visible={this.state.loading} 
+                    textContent={"Loading..."} 
+                    textStyle={{color: '#FFF'}} />
+                <Content>
+                    <Form>
+                        <Item first>
+                            <Icon active name='person' style={{color:'#EBEDD0'}} />
+                            <Input name="loginEmail"
+                                   placeholder="Email"
+                                   placeholderTextColor='#bdc3c7'
+                                   selectionColor='#ffffff'
+                                   value={this.state.username}
+                                   onChangeText={(value) => this.setState({email: value})}
+                                   autoCapitalize="none"
+                                   autoCorrect={false}
+                                   style={loginStyles.fieldInput} />
+                        </Item>
+                        <Item style={loginStyles.inputSpacing}>
+                            <Icon active name='lock' style={{color:'#EBEDD0'}} />
+                            <Input name="loginPassword"
+                                   placeholder="Password"
+                                   placeholderTextColor="#bdc3c7"
+                                   selectionColor='#ffffff'
+                                   value={this.state.password}
+                                   onChangeText={(value) => this.setState({password: value})}
+                                   autoCapitalize="none"
+                                   secureTextEntry={true}
+                                   style={loginStyles.fieldInput} />
+                        </Item>
+                        <View style={loginStyles.loginSection}>
+                            <Button block
+                                    onPress={() => safeExecute(this._login)}
+                                    style={{backgroundColor:'#88B652'}}>
+                                <Text style={{color:'#EBEDD0',fontWeight:'bold'}}>Login</Text>
+                            </Button>
+                        </View>
+                        <View style={loginStyles.registerSection}>
+                            <Text style={{color:'#000000'}}>───────  or  ───────</Text>
+                            <Text style={{color:'#000000',paddingTop:20}}>Don't have an Account?</Text>
+                            <Button block
+                                    onPress={() => safeExecute(this._navigateToRegister)}
+                                    style={{backgroundColor:'#EBEDD0',marginTop:15}}>
+                                <Text style={{color:'#3F664E',fontWeight:'bold'}}>Sign Up</Text>
+                            </Button>
+                        </View>
+                        {/* if we wanna add a 'forgot password' feature
+                        <View style={loginStyles.forgotPasswordSection}>
+                            <Text style={loginStyles.forgotPasswordButton}
+                                  onPress={() => this.props.navigation.navigate('ForgotPassword')}>
+                                Forgot password?
+                            </Text>
+                        </View>
+                        */}
+                    </Form>
+                </Content>
+            </Container>
+        );
+    }
+
+}
+
+const loginStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#FFFFFF',
+        padding: 20
+    },
+    brand: {
+        fontFamily: 'Helvetica',
+        color: '#EBEDD0',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 32
+    },
+    title: {
+        fontFamily: 'Helvetica',
+        color: '#88B652',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingTop: 25,
+        paddingBottom: 10,
+        paddingLeft: 5,
+        paddingRight: 5
+    },
+    loginSection: {
+        paddingTop: 20
+    },
+    registerSection: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 25
+    },
+    forgotPasswordSection: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 20
+    },
+    forgotPasswordButton: {
+        color: 'blue',
+    },
+    inputSpacing: {
+        paddingTop: 10
+    },
+    fieldInput: {
+        color: '#ffffff'
+    }
 });
-
-const LoginScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Screen A
-    </Text>
-    <Text style={styles.instructions}>
-      This is great
-    </Text>
-    <Button
-      onPress={() => navigation.dispatch({ type: 'Login' })}
-      title="Log in"
-    />
-  </View>
-);
-
-LoginScreen.propTypes = {
-  navigation: PropTypes.object.isRequired,
-};
 
 LoginScreen.navigationOptions = {
   title: 'Log In',
 };
-
-export default LoginScreen;
---
+++ src/config/PotAuth.js
@@ -0,0 +1,219 @@
+import { AsyncStorage } from 'react-native';
+let base_64 = require('base-64');
+
+export const APP_USER_ID = "applicationUserId"; // applicationUserId decoded from id_token
+export const ACCESS_TOKEN = "access_token";
+export const REFRESH_TOKEN = "refresh_token";
+export const EXPIRES_IN = "expires_in";
+export const CREDENTIALS   = "credentials"
+
+/**
+ * Decodes id_token into applicationUserId to store into AsyncStorage.
+ * @param token (JWT)
+ * @returns {Promise.<void>}
+ */
+export const storeApplicationUserId = async (token) => {
+    try {
+        await AsyncStorage.setItem(APP_USER_ID, this._decodeJWT(token).sub);
+    } catch (error) {
+        alert(error); // error saving app user id
+    }
+};
+
+/**
+ * Stores access_token into AsyncStorage.
+ * @param token (JWT)
+ * @returns {Promise.<void>}
+ */
+export const storeAccessToken = async (token) => {
+    try {
+        await AsyncStorage.setItem(ACCESS_TOKEN, token);
+    } catch (error) {
+        alert(error); // error saving token
+    }
+};
+
+/**
+ * Stores refresh_token into AsyncStorage.
+ * @param token (JWT)
+ * @returns {Promise.<void>}
+ */
+export const storeRefreshToken = async (token) => {
+    try {
+        await AsyncStorage.setItem(REFRESH_TOKEN, token);
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Stores expires_in time into AsyncStorage.
+ * @param time (presumably in seconds)
+ * @returns {Promise.<void>}
+ */
+export const storeExpiresIn = async (time) => {
+    try {
+        await AsyncStorage.setItem(EXPIRES_IN, time);
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * 
+ */
+export const storeCredentials = async (email, password) => {
+    try {
+        await AsyncStorage.setItem(CREDENTIALS, JSON.stringify({ email, password }))
+    } catch(error) {
+        alert(error)
+    }
+}
+
+/**
+ * Gets APP_USER_ID (applicationUserId decoded from id_token).
+ * @returns ACCESS_TOKEN
+ */
+export const getApplicationUserId = async () => {
+    try {
+        const token = await AsyncStorage.getItem(APP_USER_ID);
+        if (token !== null) {
+            return token;
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Gets ACCESS_TOKEN.
+ * @returns ACCESS_TOKEN
+ */
+export const getAccessToken = async () => {
+    try {
+        const token = await AsyncStorage.getItem(ACCESS_TOKEN);
+        if (token !== null) {
+            return token;
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Gets REFRESH_TOKEN.
+ * @returns {Promise.<*>}
+ */
+export const getRefreshToken = async () => {
+    try {
+        const token = await AsyncStorage.getItem(REFRESH_TOKEN);
+
+        if (token !== null) {
+            return token;
+        }
+    } catch (error) {
+        alert(error);
+    }
+
+    return null
+};
+
+/**
+ * Gets EXPIRES_IN.
+ * @returns {Promise.<*>}
+ */
+export const getExpiresIn = async () => {
+    try {
+        const token = await AsyncStorage.getItem(EXPIRES_IN);
+        if (token !== null) {
+            return token;
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * 
+ */
+export const getCredentials = async () => {
+    let credentials = null
+
+    try {
+        credentials = await AsyncStorage.getItem(CREDENTIALS)
+
+        if (credentials !== null)
+            return credentials
+    } catch(error) {
+        alert(error)
+    } 
+    
+    return null
+}
+
+/**
+ * Removes APP_USER_ID from AsyncStorage.
+ */
+export const removeApplicationUserId = () => {
+    try {
+        AsyncStorage.removeItem(APP_USER_ID)
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Removes ACCESS_TOKEN from AsyncStorage.
+ */
+export const removeAccessToken = () => {
+    try {
+        AsyncStorage.removeItem(ACCESS_TOKEN)
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Removes REFRESH_TOKEN from AsyncStorage.
+ */
+export const removeRefreshToken = () => {
+    try {
+        AsyncStorage.removeItem(REFRESH_TOKEN)
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Removes EXPIRES_IN from AsyncStorage.
+ */
+export const removeExpiresIn = () => {
+    try {
+        AsyncStorage.removeItem(EXPIRES_IN)
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Removes Credentials from AsyncStorage.
+ */
+export const removeCredentials = () => {
+    try {
+        AsyncStorage.removeItem(CREDENTIALS)
+    } catch (error) {
+        alert(error);
+    }
+};
+
+/**
+ * Parses and decodes a JSON Web Token.
+ * @param token (JWT)
+ * @returns applicationUserId
+ * @private
+ */
+_decodeJWT = (token) => {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(base_64.decode(base64));
+};
