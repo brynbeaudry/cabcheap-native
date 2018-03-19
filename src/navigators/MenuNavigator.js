4db7e3bc12feb28cb0e753c1a83f8b67f7a9c2fa
Bryn Beaudry <babeaudry@gmail.com>
2018-03-19 16:20:31 -0700

    Left Header hamburger button opens up the sidebar drawer navigation menu

--- src/navigators/MenuNavigator.js
+++ src/navigators/MenuNavigator.js
@@ -8,20 +8,28 @@ import LoginScreen from '../screens/LoginScreen/container/LoginScreenContainer';
 import MainScreen from '../screens/MainScreen/container/MainScreenContainer';
 import ProfileScreen from '../screens/ProfileScreen/components/ProfileScreen';
 import RegisterScreen from '../screens/RegisterScreen/container/RegisterScreenContainer';
-import { SideBar } from "../components/SideBar";
+import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+import SideBar from "../components/SideBar";
 
 export const MenuNavigator = DrawerNavigator(
   {
-      Main: { screen: MainScreen},
+      Main: { screen: MainScreen },
       Profile: { screen: ProfileScreen },
       // Default config for all screens
-     //headerMode: 'none',
+      //headerMode: 'none',
   },
   {
-      //contentComponent : props => <SideBar {...props}/>,
+      contentComponent : props => <SideBar {...props}/>,
       initialRouteName : 'Main',
-      headerMode: 'screen',
-      drawerPosition: 'right',
+      navigationOptions : ({navigation}) => ({
+        headerMode: 'screen',
+      headerLeft : <Button
+        transparent
+        onPress={() => navigation.navigate("DrawerOpen")}>
+        <Icon name="menu" />
+      </Button>,
+      }),
+      drawerPosition: 'left',
       drawerOpenRoute: 'DrawerOpen',
       drawerCloseRoute: 'DrawerClose',
       drawerToggleRoute: 'DrawerToggle',
